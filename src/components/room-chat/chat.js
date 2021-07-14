4fa919e1dd04e1fa3422cebd0ade6196f3409442
Harshit <75361028+harshit-14@users.noreply.github.com>
2021-07-14 12:05:12 +0530

    Update chat.js

--- src/components/room-chat/chat.js
+++ src/components/room-chat/chat.js
@@ -95,7 +95,7 @@ export default function Chat(props)
                event.preventDefault();
                if(data)
                {
-                   console.log("props.socket===============>",props.socket)
+                  
                    const outer = document.getElementById('chatbox')
                    const newmess=document.createElement('div')
                    newmess.textContent=data
